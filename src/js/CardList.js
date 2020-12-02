a1d20151a6af3ff4d61419a982621d79ea6711a4
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-12-02 18:54:47 +0500

    edit: render only myCards

--- src/js/CardList.js
+++ src/js/CardList.js
@@ -9,18 +9,20 @@ export default class CardList {
   }
 
   render(array, userID) {
-    for (let i = 0; i < 10; i++) {
+    const myCards = array.filter(card => card.owner._id === userID);
+
+    for (let i = 0; i < myCards.length; i++) {
       const newCard = this.card.create(
-        array[i].name,
-        array[i].link,
-        array[i].likes.length,
-        array[i].owner._id,
-        array[i]._id,
-        array[i].likes
+        myCards[i].name,
+        myCards[i].link,
+        myCards[i].likes.length,
+        myCards[i].owner._id,
+        myCards[i]._id,
+        myCards[i].likes
       );
 
       //проверяю наличие своего лайка
-      const isMyLike = array[i].likes.some(function (user) {
+      const isMyLike = myCards[i].likes.some(function (user) {
         return user._id === userID;
       });
       //Если есть мой лайк, то закрасить сердечко
@@ -30,7 +32,7 @@ export default class CardList {
           .classList.add("place-card__like-icon_liked");
       }
       //Если карта - моя, то сделать корзину видимой
-      if (array[i].owner._id === userID) {
+      if (myCards[i].owner._id === userID) {
         newCard
           .querySelector(".place-card__delete-icon")
           .classList.add("place-card__delete-icon_visible");
